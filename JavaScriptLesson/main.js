aad6c0a0424a66bd6cf2bcbc5e766a4b48d909ff
yn1323 <yn1323@gmail.com>
2019-07-18 21:37:46 +0900

    資料更新

--- JavaScriptLesson/main.js
+++ JavaScriptLesson/main.js
@@ -1,6 +1,6 @@
 // id firstAlert のDOM(=HTML)を変数alertBtnに入れる
 // このときはまだ "#firstAlertClickとJSがリンクしているだけ" でクリックしても何も発生しない
-let alertBtnClick = document.getElementById("firstAlertClick");
+let alertBtnClick = document.getElementById("clickAlert");
 
 // "イベント"を感知するためのメソッド
 // "click"した時にfunctionの中身を実行
@@ -10,44 +10,49 @@ alertBtnClick.addEventListener("click", function(){
 });
 
 // ボタンにマウスオーバーした場合
-let alertBtnMouseOver = document.getElementById("firstAlertMouseOver");
+let alertBtnMouseOver = document.getElementById("hoverAlert");
 alertBtnMouseOver.addEventListener("mouseover", function () {
     // 実行内容
     alert("マウスオーバーするだけで表示!!!ちょっとだけじゃま...");
 });
 // ボタンを右クリックした場合
-let alertBtnRightClick = document.getElementById("firstAlertRightClick");
+let alertBtnRightClick = document.getElementById("rightClickAlert");
 alertBtnRightClick.addEventListener("contextmenu", function () {
     // 実行内容
     alert("右クリックで表示");
 });
 
-// テキストボックスの値を取得
+// テキストボックスの値を取得して表示
+// ボタンのHTML
 let valBtn = document.getElementById("getVal");
+// 表示する場所のHTML
+let outputVal = document.getElementById("outputVal");
 valBtn.addEventListener("click", function () {
     // テキストボックスの値を取得
-    // ↓2行の代わりに let inputText = document.getElementById("input").value でもよい
     let inputText = document.getElementById("input");
+    // ID input のvalue
     let value = inputText.value;
-    alert(value);
+    // outputVal の in の text を value にする
+    outputVal.innerText = value;
 });
 
 // 中級
 // !!!を付け加えた文字をアラートで表示
 // テキストボックスの値を取得
 let valBtnEx = document.getElementById("getValEx");
+let outputValEx = document.getElementById("outputValEx");
 valBtnEx.addEventListener("click", function () {
     // テキストボックスの値を取得
     let inputText = document.getElementById("inputEx");
     let value = inputText.value;
-    alert(value + "!!!");
+    outputValEx.innerHTML = "<strong style='color: red;'>" + value + "!!!!" + "</strong>";
 });
 
 // 上級
 let valBtnFizz = document.getElementById("getValFizz");
 valBtnFizz.addEventListener("click", function () {
     // テキストボックスの値を取得
-    let inputText = document.getElementById("inputFizz");
+    let inputText = document.getElementById("inputFizzBuzz");
     let value = inputText.value;
     // 10進数変化は今の時代はなくてもいいらしい
     if (!isNaN(value)){
